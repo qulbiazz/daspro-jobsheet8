72509672b11d67472ba45feb11d6aa8d9f2bdc31
qulbyazz <qulbyazzumy@gmail.com>
2024-10-30 20:09:52 +0700

    percobaan 2

---
+++ square23.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class square23 {
+    public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan nilai N: ");
+        int n = input.nextInt();
+
+        // for(int i = 1; i<=n; i++){
+        //     System.out.print(i+1);
+        // }
+        System.out.println();
+        for(int iOuter = 1; iOuter <= n; iOuter++){
+            for(int i = 1; i<=n; i++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
